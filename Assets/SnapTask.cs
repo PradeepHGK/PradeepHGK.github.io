a730bd20ad65ce43227c48ab1401910c23c8cd00
Pradeep HGK <pradeephari1594@gmail.com>
2023-06-06 11:11:20 +0530

    - Added perspective mode support to have the depth

--- Assets/SnapTask.cs
+++ Assets/SnapTask.cs
@@ -12,21 +12,34 @@ public class SnapTask : BaseTask, IBeginDragHandler, IEndDragHandler
     [SerializeField] GameObject _modelInstantiated;
 
     public Material _SocketMaterial;
-
-
+    private Vector3 mousePost;
 
     private void Start()
     {
         TaskType = Tasks.Snap;
     }
 
+    private void OnMouseDown()
+    {
+        mousePost = Input.mousePosition - Camera.main.WorldToScreenPoint(SnapObject.transform.position);
+    } 
+
     // Update is called once per frame
     void Update()
     {
+        //if (_modelInstantiated != null)
+        //{
+        //    var camPos = Camera.main.ScreenToWorldPoint(Input.mousePosition - mousePost);
+        //    var newPos = new Vector3(camPos.x, camPos.y, 0);
+        //    _modelInstantiated.transform.position = newPos;
+        //}
+
+
         if (_modelInstantiated != null)
         {
-            var camPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            var newPos = new Vector3(camPos.x, camPos.y, 0);
+            var mousPos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z + SnapObject.transform.position.z);
+            var newPos = Camera.main.ScreenToWorldPoint(mousPos);
+
             _modelInstantiated.transform.position = newPos;
         }
     }
